ad3572fd2801efb09259720745ac6197378d3c35
Dizagler <sid_alex@yahoo.com>
2021-10-11 08:58:17 +0300

    add new files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+  return <header className={s.header}>
+    <img src="http://pngimg.com/uploads/kite/kite_PNG44.png" alt="" />
+  </header>
+}
+
+export default Header;
